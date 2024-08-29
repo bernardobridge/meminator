7d791620aa190e9b46c49eb654c9b27d85e65b9f
Bernardo Guerreiro <bernardoguerr@gmail.com>
2024-08-29 12:42:11 +0100

    refactor: remove noise from auto-instrumentation

--- nodejs/services/backend-for-frontend-nodejs/src/tracing/auto.ts
+++ nodejs/services/backend-for-frontend-nodejs/src/tracing/auto.ts
@@ -1,6 +1,7 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { ExpressLayerType } from '@opentelemetry/instrumentation-express';
 import { SERVICE_NAME } from './custom';
 
 const sdk: NodeSDK = new NodeSDK({
@@ -10,7 +11,10 @@ const sdk: NodeSDK = new NodeSDK({
         getNodeAutoInstrumentations({
             '@opentelemetry/instrumentation-fs': {
                 enabled: false,
-            }
+            },
+            '@opentelemetry/instrumentation-express': {
+                ignoreLayersType: [ExpressLayerType.MIDDLEWARE, ExpressLayerType.REQUEST_HANDLER],
+            },
         }),
     ],
 });
